a7b2785e83c581661d5b830cc51e77af3fb5d6ec
Fastering <73803835+Fastering18@users.noreply.github.com>
2021-06-14 22:26:13 +0700

    Update gblk.py

--- gblk.py
+++ gblk.py
@@ -1,36 +1,28 @@
 import Utama as interpreter
 from pathlib import Path
-from datetime import date
 import sys
 
-tanggal = date.today().strftime("%B %d, %Y")
+def jalankan(namafile):
+    try:
+        isifile = open(namafile, encoding="utf8").read()
+        hasil, error = interpreter.esekusi(namafile, isifile)
 
-print(
-f"""
-Gblk 0.0.6 (https://github.com/Fastering18/Membuat-Bahasa-Pemrograman, {tanggal}) di {sys.platform}
-"""
-)
+        if error: 
+            print(error.jadiString())
+            sys.exit(1)
+    except UnicodeDecodeError:
+        print("Enkoding file harus berupa valid UTF-8")
+
 if len(sys.argv) > 1:
     namafile = sys.argv[1]
-    filetujuan = Path(namafile)
-    if filetujuan.is_file():
-        try:
-            isifile = open(namafile, encoding="utf8").read()
-            hasil, error = interpreter.esekusi(namafile, isifile)
-
-            if error: 
-                print(error.jadiString())
-                sys.exit(1)
-            elif hasil: 
-                if len(hasil.isi) == 1:
-                    print(repr(hasil.isi[0]))
-                else:
-                    print(repr(hasil))
-        except UnicodeDecodeError:
-            print("Enkoding file harus berupa valid UTF-8")
+    if Path(namafile).is_file():
+        jalankan(namafile)
+    elif Path(namafile + ".gblk").is_file():
+        jalankan(namafile + ".gblk")
     else:
         print("file tidak ditemukan")
 
+# jika berjalan tanpa error
 sys.exit(0)
 
-# python gblk.py
+# python gblk.py <namafile> 
